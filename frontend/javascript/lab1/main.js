2ba9d738611b1190c6849325a075e1a6ecddf5f0
m7md158 <m7md.a7md158@gmail.com>
2025-07-09 16:32:41 +0300

    js lab1

---
+++ frontend/javascript/lab1/main.js
@@ -0,0 +1,60 @@
+// task1  h1 = "h" + 1  >>  coercion
+
+// var result = prompt("Enter your message:");
+
+// for (var i=1 ; i<=6 ; i++){
+
+//     document.write("<h" + i + ">" + result + "</h" + i + ">");
+// }
+
+
+
+
+// task2
+
+// input as string , i should convert it to number 
+
+
+// var sum = 0;
+
+// while (true) {
+//     var num = prompt("Enter a number:");
+
+//     if (num === null ||  isNaN(Number(num))) {
+//         console.log("Please enter numbers only");
+//         continue;
+//     }
+
+//     var num = Number(num);
+
+//     if (num === 0) {
+//         break;
+//     }
+
+//     sum += num;
+//     console.log("Sum: " + sum);
+
+//     if (sum >= 100) {
+//         break;
+//     }
+// }
+
+// alert("Total sum of entered values is: " + sum);
+
+
+
+
+
+
+//  task3
+
+
+// var input = prompt("enter the string: ");
+// var count = 0;
+
+// for (var i = 0; i < input.length; i++) {
+//     if ( input[i] === "e") {
+//         count++;
+//     }
+// }
+// alert("Number of 'e' characters: " + count);
